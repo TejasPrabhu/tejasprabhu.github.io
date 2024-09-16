147c34655feb1e8347032561a923ef37b5a00b05
Tejas <tejas.prabhu29@gmail.com>
2024-09-16 18:26:11 -0400

    fix slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -270,3 +270,18 @@ var swiper = new Swiper('.blog-slider', {
     clickable: true,
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  new Swiper('.experience-slider', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
